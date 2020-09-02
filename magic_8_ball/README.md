822716e1fb2e2647e9fef1f01f6ea63dba1b6a15
Clint Pedersen <cpedersen@verisk.com>
2020-09-01 22:46:50 -0600

    Minor changes.

---
+++ magic_8_ball/README.md
@@ -0,0 +1,2 @@
+## Magic 8 Ball
+Just a couple simple magic 8 ball applications for AWS Lamba.
--- magic_8_ball/magic_8_ball.py
+++ magic_8_ball/magic_8_ball.py
@@ -4,18 +4,19 @@ import time
 
 
 seconds_since_epoch = calendar.timegm(time.gmtime())
-seed(seconds_since_epoch)
+def handler_name(event, context):
+    seed(seconds_since_epoch)
 
-switch = {
-    1: "Ask again later",
-    2: "Concentrate and ask again",
-    3: "It is certain",
-    4: "My reply is no",
-    5: "My sources say no",
-    6: "Outlook good",
-    7: "Reply hazy, try again",
-    8: "Something went wrong...",
-    9: "You may rely on, it"
-}
+    switch = {
+        1: "Ask again later",
+        2: "Concentrate and ask again",
+        3: "It is certain",
+        4: "My reply is no",
+        5: "My sources say no",
+        6: "Outlook good",
+        7: "Reply hazy, try again",
+        8: "Yes. No. Maybe? I dunno...",
+        9: "You may rely on, it"
+    }
 
-print(switch[randint(0, len(switch))])
+    return switch[randint(1, len(switch))]
